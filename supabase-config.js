6ae62742515d28baf49bda258a73841d7f00688b
Pedro Barbosa Farias <80399006+Pedro-Farias-301@users.noreply.github.com>
2026-06-11 19:56:37 -0300

    atualização

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://supabase.com/dashboard/project/nnlqgmtwahantjsmmohb/settings/general';
+const SUPABASE_URL = 'https://nnlqgmtwahantjsmmohb.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_7pOymjtINxlXfiBe_b6ITQ_js-_eFR_';
 
 const PRECO_POR_LITRO = 0.005;
